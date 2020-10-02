34bac9f6fc150939c9cf43eec4411975e35e4229
tanyaa0706 <tanya.7june98@gmail.com>
2020-10-02 15:38:30 +0530

    initial commit

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from  './material/material.module';
+
+import { AddTrainingComponent } from './add-training/add-training.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTrainingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
