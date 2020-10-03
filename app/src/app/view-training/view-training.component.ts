2529546cec05ca37d0326e1cb281e65a8138a98f
tanyaa0706 <tanya.7june98@gmail.com>
2020-10-03 17:08:48 +0530

    icons fixed

--- app/src/app/view-training/view-training.component.ts
+++ app/src/app/view-training/view-training.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-training.component.css']
 })
 export class ViewTrainingComponent implements OnInit {
-  displayedColumns = ['id', 'trainingName', 'facultyName', 'courseName', 'startDate', 'endDate'];
+  displayedColumns = ['id', 'trainingName', 'facultyName', 'courseName', 'startDate', 'endDate','actions'];
   dataSource = ELEMENT_DATA;
 
   constructor(private router: Router) { }
